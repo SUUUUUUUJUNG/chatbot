79ba973f1bb1d1c5944ac8aa764f5798a3e97a51
SHIN SUJUNG <shinssjj79@gmail.com>
2025-01-09 06:28:50 +0900

    Add ChatbotController with test endpoint

---
+++ src/main/java/com/example/chatbot/ChatbotController.java
@@ -0,0 +1,20 @@
+package com.example.chatbot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatbotController {
+    private final ChatbotService chatbotService;
+
+    @Autowired
+    public ChatbotController(ChatbotService chatbotService) {
+        this.chatbotService = chatbotService;
+    }
+
+    @GetMapping("/test")
+    public String testRestTemplate() {
+        return chatbotService.fetchSampleData();
+    }
+}
